80fefa04d9d1345b6c7a390ee9317609a6ab0a41
Nav <navya.suri13@gmail.com>
2018-05-01 21:53:34 +0400

    Added eraser and size, empty

--- DSH3/DSH3/binaryTree1.cpp
+++ DSH3/DSH3/binaryTree1.cpp
@@ -8,10 +8,39 @@
 
 #include "binaryTree1.hpp"
 
-int SearchTree::size() const{
+int SearchTree::size() const
+{
     return n;
 }
 
-bool SearchTree::empty() const{
-    return n==0;
+bool SearchTree::empty() const
+{
+    return n == 0;
+}
+
+typedef typename BinaryTree::Position TPos;
+TPos SearchTree::eraser(TPos &v)
+{
+    TPos w;
+    if (v.left().isExternal())
+        w = v.left(); // remove from left
+    else if (v.right().isExternal())
+        w = v.right(); // remove from right
+    else
+    {                  // both internal?
+        w = v.right(); // go to right subtree
+        do
+        {
+            w = w.left();
+        } while (!w.isExternal()); // get leftmost node
+        TPos u = w.parent();
+        v−> setKey(u−> key());
+        v−> setValue(u−> value()); // copy w’s parent to v
+    }
+    n−−;                             // one less entry
+    return T.removeAboveExternal(w); // remove w and parent
+}
+
+void SearchTree::erase(const int &k)
+{
 }
