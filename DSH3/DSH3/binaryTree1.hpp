e284ed8d4f15e7ba82d9956b63638aca91221b1f
Maria Jaramillo <mariajaramillogv@gmail.com>
2018-05-01 19:28:36 +0400

    wip

--- DSH3/DSH3/binaryTree1.hpp
+++ DSH3/DSH3/binaryTree1.hpp
@@ -10,46 +10,50 @@
 #define binaryTree1_hpp
 
 #include <stdio.h>
-template<typename K, typename V>
-class Entry{
-    typedef K Key;
-    typedef V Value;
-public:
-    Entry(const K& k=K(),const V& v=V()):_key(k),_value(v){}
+using namespace std
+#include <string> 
+
+class Entry
+{
+    int Key;
+    string Value;
+
+  public:
+    Entry(const int &k = int(), const string &v = string()) : _key(k), _value(v) {}
 };
 
-class SearchTree {
-public:
-class Iterator;
-public: 
-SearchTree();
-int size() const; 
-bool empty() const; 
-Iterator find(const int& k);
-Iterator insert(const int& k, const string& x);
-void erase(const int& k) throw(NonexistentElement);
-void erase(const Iterator& p);
-Iterator begin(); 
-Iterator end(); 
-protected:
-typedef BinaryTree<int, string> BinaryTree;
-typedef typename BinaryTree::Position TPos; 
-TPos root() const; 
-TPos finder(const int& k, const TPos& v);
-TPos inserter(const int& k, const string& x);
-TPos eraser(TPos& v); 
-TPos restructure(const TPos& v) 
-throw(BoundaryViolation);
-private: 
-BinaryTree T; 
-int n;
-public:
-// . . .insert Iterator class declaration here
-// I made a change
+class SearchTree
+{
+  public:
+    class Iterator;
+
+  public:
+    SearchTree();
+    int size() const;
+    bool empty() const;
+    Iterator find(const int &k);
+    Iterator insert(const int &k, const string &x);
+    void erase(const int &k) throw(NonexistentElement);
+    void erase(const Iterator &p);
+    Iterator begin();
+    Iterator end();
+
+  protected:
+    typedef BinaryTree<Entry> BinaryTree;
+    typedef typename BinaryTree::Position TPos;
+    TPos root() const;
+    TPos finder(const int &k, const TPos &v);
+    TPos inserter(const int &k, const string &x);
+    TPos eraser(TPos &v);
+    TPos restructure(const TPos &v) throw(BoundaryViolation);
+
+  private:
+    BinaryTree T;
+    int n;
+
+  public:
+    // . . .insert Iterator class declaration here
+    // I made a change
 };
-
-
-
-
 
 #endif /* binaryTree1_hpp */
