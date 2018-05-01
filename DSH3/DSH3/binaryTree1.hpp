fa84c9abb3223f9aa8c1fe63afe69fdcfa4a10d0
Maria Jaramillo <mariajaramillogv@gmail.com>
2018-05-01 19:08:11 +0400

    bye part 1

--- DSH3/DSH3/binaryTree1.hpp
+++ DSH3/DSH3/binaryTree1.hpp
@@ -16,8 +16,36 @@ class Entry{
     typedef V Value;
 public:
     Entry(const K& k=K(),const V& v=V()):_key(k),_value(v){}
-    
-    
+};
+
+class SearchTree {
+public:
+class Iterator;
+public: 
+SearchTree();
+int size() const; 
+bool empty() const; 
+Iterator find(const int& k);
+Iterator insert(const int& k, const string& x);
+void erase(const int& k) throw(NonexistentElement);
+void erase(const Iterator& p);
+Iterator begin(); 
+Iterator end(); 
+protected:
+typedef BinaryTree<int, string> BinaryTree;
+typedef typename BinaryTree::Position TPos; 
+TPos root() const; 
+TPos finder(const int& k, const TPos& v);
+TPos inserter(const int& k, const string& x);
+TPos eraser(TPos& v); 
+TPos restructure(const TPos& v) 
+throw(BoundaryViolation);
+private: 
+BinaryTree T; 
+int n;
+public:
+// . . .insert Iterator class declaration here
+// I made a change
 };
 
 
