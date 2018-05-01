61ea430530b8d9d154ea10bef56e60222988b159
Nav <navya.suri13@gmail.com>
2018-05-01 18:10:30 +0400

    Part 1, ADT added

---
+++ part1.hpp
@@ -0,0 +1,31 @@
+template <typename E>
+class SearchTree { 
+public: 
+typedef typename E::Key K; 
+typedef typename E::Value V; 
+class Iterator;
+public: 
+SearchTree();
+int size() const; 
+bool empty() const; 
+Iterator find(const K& k); 
+Iterator insert(const K& k, const V& x); 
+void erase(const K& k) throw(NonexistentElement); 
+void erase(const Iterator& p);
+Iterator begin(); 
+Iterator end(); 
+protected:
+typedef BinaryTree<E> BinaryTree; 
+typedef typename BinaryTree::Position TPos; 
+TPos root() const; 
+TPos finder(const K& k, const TPos& v); 
+TPos inserter(const K& k, const V& x); 
+TPos eraser(TPos& v); 
+TPos restructure(const TPos& v) 
+throw(BoundaryViolation);
+private: 
+BinaryTree T; 
+int n;
+public:
+// . . .insert Iterator class declaration here
+};
